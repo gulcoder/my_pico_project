3d3f8ee383150eab6d3600778976a948e85b7bb5
Gul Baran <gulbaran243@gmail.com>
2025-03-20 13:20:35 +0100

    försra versionen av blinkande LED på Pico

---
+++ build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/Users/gulbaran/pico/my_pico_project/build/_deps/picotool/"
+};
---
+++ build/_deps/picotool-build/lib/mbedtls/library/CMakeFiles/mbedx509.dir/pkcs11.c.o.d
@@ -0,0 +1,14 @@
+lib/mbedtls/library/CMakeFiles/mbedx509.dir/pkcs11.c.o: \
+  /Users/gulbaran/pico-sdk/lib/mbedtls/library/pkcs11.c \
+  /Users/gulbaran/pico-sdk/lib/mbedtls/include/mbedtls/pkcs11.h \
+  /Users/gulbaran/pico-sdk/lib/mbedtls/include/mbedtls/config.h \
+  /Users/gulbaran/pico-sdk/lib/mbedtls/include/mbedtls/check_config.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/limits.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/usr/include/limits.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/usr/include/sys/cdefs.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/usr/include/sys/_symbol_aliasing.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/usr/include/sys/_posix_availability.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/usr/include/machine/limits.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/usr/include/i386/limits.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/usr/include/i386/_limits.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/usr/include/sys/syslimits.h
